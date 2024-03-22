2c2478db2a108a9d771b18fb7cfa5e3eaeb3251b
zfleury <zach@fleury.lol>
2024-03-22 17:10:47 -0500

    tab select layout

--- app/home/TabSelect.tsx
+++ app/home/TabSelect.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <ul>
+    <ul className="flex justify-center space-x-3 p-3">
       <li>All</li>
+      <li>-</li>
       <li>Collected</li>
+      <li>-</li>
       <li>Uncollected</li>
+      <li>-</li>
       <li>Unreleased</li>
     </ul>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import prisma from '@/prisma/client';
 import { Card } from '@prisma/client';
 import CardTile from './collection/CardTile';
-import Image from 'next/image';
-import classnames from 'classnames';
+import TabSelect from './home/TabSelect';
 export default async function Home(card: Card) {
   const cards = await prisma.card.findMany();
   return (
-    <main className="flex ">
-      {/* <h1>Dan Hipp Collection Tracker</h1> */}
-      <div className="grid sm:grid-cols-3 md:grid-cols-5 gap-4">
-        {cards.map((card) => (
-          <CardTile key={card.id} {...card} />
-        ))}
+    <main className="flex flex-col">
+      <div className="text-center">
+        <h1 className="text-xl font-bold py-5">Dan Hipp Collection Tracker</h1>
+        <div className="m-auto">
+          <TabSelect />
+        </div>
+      </div>
+      <div className="m-auto">
+        <div className="grid sm:grid-cols-3 md:grid-cols-5 gap-4">
+          {cards.map((card) => (
+            <CardTile key={card.id} {...card} />
+          ))}
+        </div>
       </div>
     </main>
   );
